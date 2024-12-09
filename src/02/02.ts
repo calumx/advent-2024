a69ce2722d566816c81d20e3a0e6327e634b5a90
Calum Muir <calum@calumx.com>
2024-12-09 19:43:38 +0000

    day 2 part 2

--- src/02/02.ts
+++ src/02/02.ts
@@ -1,31 +1,53 @@
+const isReportValid = (report: number[]) => {
+  const firstNum = report[0]
+  const secondNum = report[1]
+
+  if (!firstNum || !secondNum) return false
+
+  let direction: 'up' | 'down' = firstNum < secondNum ? 'up' : 'down'
+
+  for (let i = 0; i < report.length - 1; i++) {
+    const num = report[i]
+    const nextNum = report[i + 1]
+    if (!num || !nextNum) return false
+    if (direction === 'up' && num >= nextNum) return false
+    if (direction === 'down' && num <= nextNum) return false
+    const distance = Math.abs(num - nextNum)
+    if (distance < 1 || distance > 3) return false
+  }
+
+  return true
+}
+
 export function parse(input: string) {
-  return input
-    .split('\n')
-    .toSpliced(-1)
-    .map(report => report.split(' ').map(Number))
+  return input.split('\n').map(report => report.split(' ').map(Number))
 }
 
 export function partOne(input: ReturnType<typeof parse>) {
-  return input.filter(report => {
-    const firstNum = report[0]
-    const secondNum = report[1]
-
-    if (!firstNum || !secondNum) return false
-
-    let direction: 'up' | 'down' = firstNum < secondNum ? 'up' : 'down'
-
-    for (let i = 0; i < report.length - 1; i++) {
-      const num = report[i]
-      const nextNum = report[i + 1]
-      if (!num || !nextNum) return false
-      if (direction === 'up' && num >= nextNum) return false
-      if (direction === 'down' && num <= nextNum) return false
-      const distance = Math.abs(num - nextNum)
-      if (distance < 1 || distance > 3) return false
-    }
-
-    return true
-  }).length
+  return input.filter(isReportValid).length
 }
 
-export function partTwo(input: ReturnType<typeof parse>) {}
+export function partTwo(input: ReturnType<typeof parse>) {
+  const valid: number[][] = []
+  const invalid: number[][] = []
+
+  input.forEach(report => {
+    if (isReportValid(report)) {
+      valid.push(report)
+    } else {
+      invalid.push(report)
+    }
+  })
+
+  invalid.forEach(report => {
+    for (let i = 0; i <= report.length - 1; i++) {
+      const modified = report.toSpliced(i, 1)
+      if (isReportValid(modified)) {
+        valid.push(modified)
+        break
+      }
+    }
+  })
+
+  return valid.length
+}
